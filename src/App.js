c7c60f9e7f098273deed9ea149b0719399810875
Good-ho <whdxo100@gmail.com>
2020-10-16 22:19:27 +0900

    Class Components and State
    	1. State는 보통 동적 데이터 다룰 때 사용. 변하는 데이터
    
    	2. Class component로 바꿔보자 React.Component는 필수다. (이 안에 state도 관련 된 것들도 있음)
    	- Class는 function과 다르게 render를 가지고 그 안에 return 있음.
    	- React는 자동으로 class component의 render를 수행하려함.
    	- 왜 갑자기 class를 쓰냐 ? State를 쓰기 위해
    
    	3. State는 object다.
    	- State의 count를 선언하고 add btn 클릭시 이 값을 증가시켜보자.
    	- 여기서 this.state.count +=1 이런식으로 state에 direct 접근하면 안된다!
    	- 만약 direct 접근을해서 변경하면, react는 render function을 refresh하지 않음.
    	- setState를 통해 값을 변경해야 react는 다시 render한다 ! Owesome!
    	- State는 object이므로 아래와 같이 값을 전달해야함
    	this.setState({count:1})
    	- setState를 하면 render을 다시하고 변화된 element만 다시 그린다 !
    	이것은 virtual DOM 때문에 가능한 것으로, virtual DOM은 이전 상태값을 갖고 있고 render되면 현재 가상으로 갖고있는 DOM 값과 이전 가상 DOM을 비교하여 변화된 부분만 다시 그림. 그래서 깜빡임 없음.
    	
    	- setState를 아래와 같이도 사용가능.
    	현재 상태에 값을 바꾸는 것이지.
    this.setState(current => ({count:current.count+1}));

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, rating }) {
-  return <h2> Food ! {name} {rating}</h2>;
-}
+class App extends React.Component{
+  state = {
+    count:0
+  }
 
-Food.propTypes = {
-  name:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-}
-
-const foodArray = [
-  { name: "kimchi", id: 1, rating:3 },
-  { name: "ramen", id: 2, rating:2.9 },
-  { name: "kimbob", id: 3, rating:4.5 },
-];
-
-function App() {
+  onHandleAdd = () => {
+    // this.state.count += 1;
+    // this.setState({count: this.state.count+1})
+    this.setState(current => ({count:current.count+1}));
+  }
+  
+  onHandleMin = () => {
+    this.setState({count: this.state.count-1})
+  }
+  
+  render(){
   return (
     <div>
-      {foodArray.map((food) => (
-        <Food key={food.id} name={food.name} rating={food.rating} />
-      ))}
+      <h1> number : {this.state.count}</h1>
+      <button onClick={this.onHandleAdd}>Add</button>      
+      <button onClick={this.onHandleMin}>Min</button>      
     </div>
-  );
+  )
+  }
 }
 
 export default App;
