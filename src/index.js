a189300165250fa57ae78b1efc613cd2e8d24188
Good-ho <whdxo100@gmail.com>
2020-10-16 17:11:28 +0900

    Creating your first React Component
    	1. 아래 코드는 HTML과 비슷해 보이지만 HTML이 아니다.
    	- App.js의 <div> 요소는 html이 맞지, but! 
    	- 기본적으로 아래 코드 <App/>을 component라 부른다.
    	- Component는 기본적으로 HTML을 반환하는 함수다.
    	- 즉, component가 html을 반환하고 그것을 render해줌.
    	- Javascript와 HTML간의 이러한 조합을 JSX라 한다.
    	- Jsx는 react에 나온 매우 custom한 개념임.
    React가 소개한 유일한 개념
    
    	2. 나만의 component 만들어보기.
    	- src/Potato.js 파일 생성.
    	- 아래와 같이 코드 작성.
    	- Potato function을 만들어 html을 return 해주고 index.js에서 potato component를 호출하면됨.
    	- 그럼 react에서 component에서 brower가 이해할수 있는 html로 만들어서 넣어줌.
    	- oweSome!
    	
    import React from "react";
    function Potato(){
        return (
            <h3>i love potato</h3>
        );
    }
    export default Potato;
    
    
    	3. JSX
    Jsx는 javascript안의 html 임.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Potato from './Potato';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Potato/>
   </React.StrictMode>,
   document.getElementById('root')
 );
