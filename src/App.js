14275afe0e49fd03b2b40581abd800b81d0d850c
Good-ho <whdxo100@gmail.com>
2020-10-16 17:41:16 +0900

    Reusable Components with JSX + Props 
    	1. JSX 첫 번째 이해
    	- Component가 html을 반환하고 react가 반환된 html을 render한다.
    	- 즉, jsx는 js안의 html같은 것.
    
    	2. Jsx 두 번째 이해.
    	- Component에 정보를 보낼 수 있다.
    	- React가 멋인 이유는 재사용 가능한 component를 만들 수 있다는 것.
    
    	3. 2번에서 말한 component에 어떻게 정보를 보내는지?
    	- 아래 코드와 같이 food에 name값을 전달해주는 것은 food component에 kimchi라는 value로 prop name을 전달 한 것.
    	function App() {
    	  return (
    	    <div>
    	      <h1>Hello!</h1>
    	      <Food name="kimchi"/>
    	    </div>
    	    
    	  );
    	}
    	- 아래와 같이 food component는 전달된 prop을 전달 받을 수 있다.
    	function Food(props){
    	  console.log(props);
    	  return (
    	    <h2> Potato !</h2>
    	  )
    	}
    	
    	function Food({name}){  
    	  return (
    	  <h2> Food ! {name}</h2>
    	  )
    	}
    	
    	- 아래 코드와 같이 jsx + props의 힘으로 모두 재사용가능!!
    	function App() {
    	  return (
    	    <div>
    	      <h1>Hello!</h1>
    	      <Food name="kimchi"/>
    	      <Food name="aaa"/>
    	      <Food name="bbb"/>
    	    </div>
    	    
    	  );
    	}

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+function Food({name}){  
+  return (
+  <h2> Food ! {name}</h2>
+  )
+}
+
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <h1>Hello!</h1>
+      <Food name="kimchi"/>
+      <Food name="aaa"/>
+      <Food name="bbb"/>
+    </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Potato from './Potato';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Potato/>
+    <App />    
   </React.StrictMode>,
   document.getElementById('root')
 );
