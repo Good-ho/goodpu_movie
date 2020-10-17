7dba7cb169eeaae276cbb5cdc0eb6f8ea2559385
Good-ho <whdxo100@gmail.com>
2020-10-17 16:25:18 +0900

    Building the Navigation
    1. Link는 router안에 있어야함.
    2. Link는 href와 다르게 to를 사용

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import {HashRouter, Route} from "react-router-dom";
 import About from "./routers/About";
 import Home from "./routers/Home";
+import Navigation from "./component/Nagivation";
 
 function App(){
   return(
     <HashRouter>
+      <Navigation/>
       <Route path="/" exact={true} component={Home}/>
       <Route path="/about" component={About}/>
     </HashRouter>
