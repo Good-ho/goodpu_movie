0b6bf8c6d9a860d8f5cce434dcebd97b5a6a2730
Good-ho <whdxo100@gmail.com>
2020-10-17 09:55:17 +0900

    Fetching Movies from API 
    	1. componentDidmount후 fetch를 통해 data 받으면 되지만,, fetch보다는 axios 사용이 좋음.
    	- Axios는 fetch위에 있는 작은 layer
    	- yarn add axios
    
    	2. YTS에서 movie data를 가져와 보자.
    	- http://yts.am
    	- https://yts.mx/api#list_movies
    
    	3. Axios를 통해 data를 받아올 때 awiat를 해야함
    Axios는 다소 시간이 걸리므로, async - awiat를 통해 기다리게 한다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios"
 import PropTypes from "prop-types";
 
 class App extends React.Component {
@@ -7,10 +8,12 @@ class App extends React.Component {
     movies : []
   }
 
+  getMovie = async () => {
+    const movie = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+
   componentDidMount = () => {
-    setTimeout(()=>{
-      this.setState({isLoading:false})
-    }, 5000)
+    this.getMovie();
   }
 
   render() {
