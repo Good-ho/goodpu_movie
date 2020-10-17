a8b3a7132d1d9bd6aaad4e39745d997242844e21
Good-ho <whdxo100@gmail.com>
2020-10-17 15:23:24 +0900

    Updates

---
+++ precache-manifest.c02664068944378f1dc84b5fd51f978f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7cf0cb273f398ea5b51a74e7b9b20c",
+    "url": "/goodpu_movie/index.html"
+  },
+  {
+    "revision": "17ec7367542e8cb4166e",
+    "url": "/goodpu_movie/static/css/main.f3581839.chunk.css"
+  },
+  {
+    "revision": "230ce775f9899213feec",
+    "url": "/goodpu_movie/static/js/2.da6e788b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goodpu_movie/static/js/2.da6e788b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ec7367542e8cb4166e",
+    "url": "/goodpu_movie/static/js/main.288b9da2.chunk.js"
+  },
+  {
+    "revision": "3850c5843531028d6446",
+    "url": "/goodpu_movie/static/js/runtime-main.b48e7487.js"
+  }
+]);
